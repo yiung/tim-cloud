eb9dfac5fd01f965c82850f8629c89047beceb13
vanya <uiok@sina.com>
2020-08-24 14:51:49 +0800

    Update wechat controller root.

---
+++ config/config-center/src/main/java/com/yiung/config/configcenter/wechat/WechatConfig.java
@@ -0,0 +1,6 @@
+package com.yiung.config.configcenter.wechat;
+
+public class WechatConfig {
+    public static final String WECHAT_MINI_PROGRAM_APP_ID = "";
+    public static final String WECHAT_MINI_PROGRAM_SECRET = "";
+}
